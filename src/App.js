8e5d990b3f6d22996b74f4353587fdb40f33a5f9
ingjuanbejarano <jcbj1292@gmail.com>
2020-11-10 15:14:59 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,53 @@
+import React, { useState, useEffect, createRef } from 'react';
+import $ from 'jquery';
+import 'jquery-ui/ui/widgets/sortable';
+import 'jquery-ui/themes/base/all.css';
 import logo from './logo.svg';
 import './App.css';
+
+const Sortable = () => {
+  const ref = createRef();
+  const [$node, set$node] = useState(null);
+
+  useEffect(() => {
+    const el = ref.current;
+    const $node = $(el).sortable({
+      opacity: 0.8,
+    });
+    set$node($node);
+
+    return () => {
+      console.log('cierra pues');
+      $node.sortable('destroy');
+    };
+  }, []);
+
+  return (
+    <ul id="sortable" ref={ref}>
+      <li className="ui-state-default">
+        <span className="ui-icon ui-icon-arrowthick-2-n-s"></span>Item 1
+      </li>
+      <li className="ui-state-default">
+        <span className="ui-icon ui-icon-arrowthick-2-n-s"></span>Item 2
+      </li>
+      <li className="ui-state-default">
+        <span className="ui-icon ui-icon-arrowthick-2-n-s"></span>Item 3
+      </li>
+      <li className="ui-state-default">
+        <span className="ui-icon ui-icon-arrowthick-2-n-s"></span>Item 4
+      </li>
+      <li className="ui-state-default">
+        <span className="ui-icon ui-icon-arrowthick-2-n-s"></span>Item 5
+      </li>
+      <li className="ui-state-default">
+        <span className="ui-icon ui-icon-arrowthick-2-n-s"></span>Item 6
+      </li>
+      <li className="ui-state-default">
+        <span className="ui-icon ui-icon-arrowthick-2-n-s"></span>Item 7
+      </li>
+    </ul>
+  );
+};
 
 function App() {
   return (
@@ -18,6 +66,7 @@ function App() {
           Learn React
         </a>
       </header>
+      <Sortable />
     </div>
   );
 }
